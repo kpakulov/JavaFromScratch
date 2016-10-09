97cd6c90afca7b33a0622938a86e267c9cd35725
kpakulov <a78497138@gmail.com>
2016-10-08 23:42:03 -0400

    Perestroika phase one

---
+++ part1/src/main/java/ru/kpakulov/calculator/Calculate.java
@@ -0,0 +1,10 @@
+/**
+* Calculate - a Hello World program
+* @author Konstantin Pakulov
+* @version October 10 2016
+*/
+public class Calculate {
+    public static void main (String [] args){
+        System.out.println("Hello World!");
+    }  
+}
