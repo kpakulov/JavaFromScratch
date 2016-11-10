3ee896c6e81c426d95a7a00d867565b145d25e7a
kpakulov <a78497138@gmail.com>
2016-11-10 00:47:52 -0500

    as of 11/12/2016

---
+++ part1/src/main/java/ru/kpakulov/square/Square.java
@@ -0,0 +1,38 @@
+package ru.kpakulov.quadratic;
+
+/**
+ *  Class Square which calculates the quadratic equation y = a*x2 + b * x + c
+ *
+ *  @author Konstantin Pakulov
+ *  @version 11/04/2016
+ */
+public class Square {
+    
+    private double a;
+    private double b;
+    private double c;
+    
+    public Square (double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;     
+    }
+
+    
+    public double quadraEq(int x){
+        return (this.a * Math.pow(x, 2) + this.b * x + this.c);
+        
+    }
+    
+    public void show(String[] args){
+        
+        int xlow = Integer.valueOf(args[0]);
+        int xhigh = Integer.valueOf(args[1]);
+        int step = Integer.valueOf(args[2]);
+        
+    //    Square quadratic = new Square(a, b, c);
+        for(int i = xlow; i <= xhigh; i = i + step){
+            System.out.println(quadraEq(i));
+        }
+    }
+}
---
+++ part1/src/test/java/ru/kpakulov/square/SquareTest.java
@@ -0,0 +1,43 @@
+package ru.kpakulov.quadratic;
+
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+/**
+ * Testing Quadratic class
+ *
+ * @author Konstantin Pakulov
+ * @version 11/7/2016
+ */
+
+
+//@Ignore
+
+public class SquareTest {
+
+    @Test
+    public void whenSettingABCThenQuadraticExpected() throws Exception {
+        Square quadra = new Square(1,2,3);
+        double quadraResult = quadra.quadraEq(4);
+        assertThat(quadraResult, is(27.0));
+    }
+    
+    @Test
+    public void whenMainIsPrintingThenCatchOutput() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        
+        Square quadra = new Square(1,2,3);
+        String arrgs[] = {"6", "7", "1"};
+        quadra.show(arrgs);
+        assertThat(out.toString(), is("51.0\n66.0\n"));
+    }
+}
